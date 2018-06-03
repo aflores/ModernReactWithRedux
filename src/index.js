5cc29ef5d42b265c631d4b5742fa070091e42fb0
Armando Flores <aflores57@gmail.com>
2018-06-03 11:42:18 -0400

    a simple use of routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
+
+class Hola extends React.Component {
+  render() {
+    return <h1>Hola</h1>;
+  }
+}
+
+class Adios extends React.Component {
+  render() {
+    return <h1>Adios</h1>;
+  }
+}
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <BrowserRouter>
+      <div>
+        <Route path="/hola" component={Hola} />
+        <Route path="/adios" component={Adios} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('.container'),
+);
